91296009a62f23cacd0abb787b0d35029c08b75e
n0bisuke <rsksound@gmail.com>
2015-12-25 17:12:01 +0900

    milkcocoa

--- milkcocoa/app.js
+++ milkcocoa/app.js
@@ -3,25 +3,32 @@
 let milkcocoa = new MilkCocoa('dogi9jz8c16.mlkcca.com');
 let ds = milkcocoa.dataStore("dots");
 
-document.querySelector('#connect').addEventListener('click', event => {
-  document.querySelector('#state').classList.add('connecting');
-  playbulbCandle.connect()
-  .then(() => {
-    console.log(playbulbCandle.device);
-    document.querySelector('#state').classList.remove('connecting');
-    document.querySelector('#state').classList.add('connected');
-    return playbulbCandle.getDeviceName().then(handleDeviceName)
-    .then(() => playbulbCandle.getBatteryLevel().then(handleBatteryLevel));
-  })
-  .catch(error => {
-    console.error('Argh!', error);
-  });
-});
+// document.querySelector('#connect').addEventListener('click', event => {
+//   document.querySelector('#state').classList.add('connecting');
+//   playbulbCandle.connect()
+//   .then(() => {
+//     console.log(playbulbCandle.device);
+//     document.querySelector('#state').classList.remove('connecting');
+//     document.querySelector('#state').classList.add('connected');
+//     return playbulbCandle.getDeviceName().then(handleDeviceName)
+//     .then(() => playbulbCandle.getBatteryLevel().then(handleBatteryLevel));
+//   })
+//   .catch(error => {
+//     console.error('Argh!', error);
+//   });
+// });
 
-ds.on('push', (pushed)=>{
-  console.log(pushed.value.color);
-  playbulbCandle.setColor(255, 0, 0).then(onColorChanged);
-})
+function hexToRGB(color){
+  let r = parseInt(color.substr(1,2), 16);
+  let g = parseInt(color.substr(3,2), 16);
+  let b = parseInt(color.substr(5,2), 16);
+  return [r,g,b];
+}
+
+ds.on('push', (pushed) => {
+  // console.log(r,g,b);
+  playbulbCandle.setColor(hexToRGB(pushed.value.color)).then(onColorChanged);
+});
 
 // function changeColor(){
 //   // playbulbCandle.setColor(r, g, b).then(onColorChanged);
